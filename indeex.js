5176664d7652371f5ccf46bcd4712dd8e67be947
maryamr25 <maryamrasheed0987@gmail.com>
2024-03-01 17:06:37 +0500

    task 2

---
+++ indeex.js
@@ -0,0 +1,39 @@
+// Calculate Total Cost
+// Write a function called calculateTotalCost that takes two arguments: price (the price of an item) and quantity (the quantity of the item purchased). The function should calculate and return the total cost by multiplying the price and quantity. Test the function with different price and quantity values.
+// function calculateTotalCost(){
+//     let priceItems = +prompt("enter price of items");
+//     let quantityItems = +prompt("enter quantity of the item purchased");
+//     return priceItems * quantityItems;
+// }
+// console.log(calculateTotalCost());
+
+// 2.Calculate Area of Rectangle
+// Develop a function called calculateArea that accepts two arguments: length and width of a rectangle. The function should calculate and return the area of the rectangle using the formula: area = length * width. Test the function with various length and width values.
+
+// function calculateArea(){
+//     let width = 50 ;
+//     let length = 70 ;
+//     return area = width * length;
+// }
+// console.log(calculateArea());
+
+// 3.Calculate BMI (Body Mass Index) 
+// Write a function called calculateBMI that takes two arguments: weight (in kilograms) and height (in meters). The function should calculate and return the BMI using the formula: BMI = weight / (height * height). Test the function with different weight and height values.
+// function calculateBMI(){
+//   let weight = +prompt("Enter weight (in kilograms)");
+//   let height = +prompt("Enter height (in meters)")
+    
+//    return BMI = weight / (height * height)
+// }
+// console.log(calculateBMI());
+
+// 4.Calculate Area of Triangle
+
+// Develop a function named calculateTriangleArea that accepts three arguments: base, height, and side. The function should calculate and return the area of a triangle using the formula: area = (base * height) / 2. Test the function with various base, height, and side values.
+
+function calculateTriangleArea(){
+   let base = +prompt("Enter the value of base");
+   let height = +prompt("Enter the value of height");
+   return triangleArea = base * height / 2;
+}
+console.log(calculateTriangleArea());
